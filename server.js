2d72834e01ee1985b34b11e0c1c44d7ed20dbc78
Henok <henokmicrometre@gmail.com>
2023-10-30 09:52:08 +0000

    added sql

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var mysql = require('mysql');
-
+const express = require('express')
+const app = express()
+const port = 3000
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "wordpressuser",
-  password: "=[-p0o9i8U",
-  database: "wordpress"
+   host: "localhost",
+   user: "wordpressuser",
+   password: "=[-p0o9i8U",
+   database: "wordpress"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM wp_posts", function (err, result, fields) {
-   if (err) throw err;
-   console.log(result);
- });
-  console.log("Connected!");
-});
+app.get('/', (req, res) => {
+   con.connect(function (err) {
+      if (err) throw err;
+      con.query("SELECT * FROM wp_posts", function (err, result, fields) {
+         if (err) throw err;
+         res.json({ "message": "success", result })
+      });
+      console.log("Connected!");
+   });
+})
+
+
+
+
+app.listen(port, () => {
+   console.log(`listening on port ${port}`)
+})
